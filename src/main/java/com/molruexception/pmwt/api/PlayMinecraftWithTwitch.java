abadf839b0a1857c8a07adf207386895de0d4bbc
molruexception@gmail.com <molruexception@gmail.com>
2023-02-20 15:00:06 +0900

    Feat: Add plugin logger

---
+++ src/main/java/com/molruexception/pmwt/api/PlayMinecraftWithTwitch.java
@@ -0,0 +1,14 @@
+package com.molruexception.pmwt.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PlayMinecraftWithTwitch {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger("PMWT");
+
+    public static Logger getLogger() {
+        return LOGGER;
+    }
+
+}
